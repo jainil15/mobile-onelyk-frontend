b4f8506c3febe5d9c73ecea8994d3bea54a33027
Jainil <jainilpatel115@gmail.com>
2024-10-22 18:18:03 +0530

    Update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,17 @@ import Support from "@/components/support";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export default function Home() {
-  return (
-    // TODO: max-w-[400px] min-w-[360px]
-
-    <section className=" w-full flex flex-col gap-5 p-4">
-      <Modal />
-
-      <AddOns />
-      
-
-      
-      <FaqComponent />
-
-      <div className="sticky bottom-0 flex  items-center  bg-background  w-full z-50 ">
-        <ScrollArea>
-          <Support />
-          <ScrollBar orientation="horizontal" />
-        </ScrollArea>
-      </div>
-    </section>
-  );
+	return (
+		<section className=" w-full flex flex-col gap-5 p-4">
+			<Modal />
+			<AddOns />
+			<FaqComponent />
+			<div className="sticky bottom-0 flex  items-center  bg-background  w-full z-50 ">
+				<ScrollArea>
+					<Support />
+					<ScrollBar orientation="horizontal" />
+				</ScrollArea>
+			</div>
+		</section>
+	);
 }
